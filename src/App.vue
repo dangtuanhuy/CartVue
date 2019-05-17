c5917c4e10a3614935a09e306f448df8e93b5b93
Huy Dang Tuan <huy.dang@orientsoftware.com>
2019-05-17 09:35:33 +0700

    s

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,36 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+	<div id="app">
+		<b-container class="bv-example-row">
+			<app-header></app-header>
+			<b-row>
+				<b-col cols="12">
+					<router-view></router-view>
+				</b-col>
+			</b-row>
+		</b-container>
+	</div>
 </template>
 
 <script>
+import Header from "./components/Header.vue";
 export default {
-  name: 'App'
-}
+	name: "App",
+	components: {
+		appHeader: Header
+	}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+/* #app {
+	font-family: "Avenir", Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+} */
+body{
+  padding: 30px;
 }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,43 @@
 <template>
- <div>
-  <h1>The Header Component</h1>
-</div>
+	<div>
+		<b-navbar toggleable="lg" type="dark" variant="dark">
+			<router-link to="/redirect-me" tag="div">
+				<b-navbar-brand>Trade Cart</b-navbar-brand>
+			</router-link>
+			<b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+			<b-collapse id="nav-collapse" is-nav>
+				<b-navbar-nav>
+					<router-link to="/portfolio" tag="div">
+						<b-nav-item>Protfolio</b-nav-item>
+					</router-link>
+					<router-link to="/stocks" tag="div">
+						<b-nav-item>Stocks</b-nav-item>
+					</router-link>
+				</b-navbar-nav>
+				<!-- Right aligned nav items -->
+				<b-navbar-nav class="ml-auto">
+					<b-nav right>
+						<router-link to="/portfolio" tag="div">
+							<b-nav-item>End Day</b-nav-item>
+						</router-link>
+					</b-nav>
+					<b-nav-item-dropdown right>
+						<!-- Using 'button-content' slot -->
+						<template slot="button-content">
+							<em>Save & Load</em>
+						</template>
+						<b-dropdown-item href="#">Save Data</b-dropdown-item>
+						<b-dropdown-item href="#">Load Data</b-dropdown-item>
+					</b-nav-item-dropdown>
+				</b-navbar-nav>
+			</b-collapse>
+		</b-navbar>
+	</div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
